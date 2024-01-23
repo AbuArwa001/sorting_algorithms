efc0fb85cf98b4d439d6db1a815bf6b2f6ea66b1
AbuArwa001 <126099630+AbuArwa001@users.noreply.github.com>
2024-01-23 11:13:57 +0000

    Sort quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -16,9 +16,11 @@ void swap(int *a, int *b)
  * partition - partitions an array into two segments based on a pivot
  * @array: array to be partitioned
  * @size: size of the array
+ * @original_size: original size of the array
+ * @org_array: ariginal array
  * Return: index of the pivot after partitioning
  */
-size_t partition(int *array, size_t size, size_t original_size,int *org_array)
+size_t partition(int *array, size_t size, size_t original_size, int *org_array)
 {
 	int pivot = array[size - 1];
 	size_t partition_index = 0, i = 0;
@@ -36,22 +38,28 @@ size_t partition(int *array, size_t size, size_t original_size,int *org_array)
 		}
 	}
 
-	
 	swap(&array[partition_index], &array[size - 1]);
 	print_array(org_array, original_size);
 	return (partition_index);
 }
-void q_sort(int *array, size_t size, size_t original_size,int *org_array)
+/**
+ * q_sort - sorts an array using the quicksort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ * @org_size: original size of the array
+ * @org_array: ariginal array
+ */
+void q_sort(int *array, size_t size, size_t org_size, int *org_array)
 {
 	if (size > 1)
 	{
-		size_t partition_index = partition(array, size, original_size,org_array);
+		size_t p_index = partition(array, size, org_size, org_array);
 
-		if (partition_index > 0)
+		if (p_index > 0)
 		{
-			q_sort(array, partition_index, original_size, org_array);
+			q_sort(array, p_index, org_size, org_array);
 		}
-		q_sort(array + partition_index + 1, size - partition_index - 1, original_size, org_array);
+		q_sort(array + p_index + 1, size - p_index - 1, org_size, org_array);
 	}
 }
 /**
@@ -63,5 +71,6 @@ void quick_sort(int *array, size_t size)
 {
 	size_t original_size = size;
 	int *org_array = array;
-	q_sort(array, size,original_size, org_array);
+
+	q_sort(array, size, original_size, org_array);
 }
