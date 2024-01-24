7d2321ccd8c889dca53fe3e5019a0c30666f08c1
AbuArwa001 <khalfanathman12@yahoo.com>
2024-01-24 03:30:10 +0300

    Checking num 10

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * swap - swaps the values of two integers
  * @a: pointer to the first integer
@@ -16,12 +15,11 @@ void swap(int *a, int *b)
  * partition - partitions an array into two segments based on a pivot
  * @array: array to be partitioned
  * @size: size of the array
- * @original_size: original size of the array
- * @org_array: ariginal array
  * Return: index of the pivot after partitioning
  */
-size_t partition(int *array, size_t size, size_t original_size, int *org_array)
+size_t partition(int *array, size_t size)
 {
+    size_t si = sizeof(array) / sizeof(array[0]);
 	int pivot = array[size - 1];
 	size_t partition_index = 0, i = 0;
 
@@ -29,43 +27,16 @@ size_t partition(int *array, size_t size, size_t original_size, int *org_array)
 	{
 		if (array[i] < pivot)
 		{
-			if (&array[i] != &array[partition_index])
-			{
-				swap(&array[i], &array[partition_index]);
-				print_array(org_array, original_size);
-			}
+			swap(&array[i], &array[partition_index]);
 			partition_index++;
 		}
 	}
 
-	if (&array[size - 1] != &array[partition_index])
-	{
-		swap(&array[partition_index], &array[size - 1]);
-		print_array(org_array, original_size);
-	}
+	swap(&array[partition_index], &array[size - 1]);
 
 	return (partition_index);
 }
-/**
- * q_sort - sorts an array using the quicksort algorithm
- * @array: array to be sorted
- * @size: size of the array
- * @org_size: original size of the array
- * @org_array: ariginal array
- */
-void q_sort(int *array, size_t size, size_t org_size, int *org_array)
-{
-	if (size > 1)
-	{
-		size_t p_index = partition(array, size, org_size, org_array);
 
-		if (p_index > 0)
-		{
-			q_sort(array, p_index, org_size, org_array);
-		}
-		q_sort(array + p_index + 1, size - p_index - 1, org_size, org_array);
-	}
-}
 /**
  * quick_sort - sorts an array using the quicksort algorithm
  * @array: array to be sorted
@@ -73,13 +44,15 @@ void q_sort(int *array, size_t size, size_t org_size, int *org_array)
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t original_size = size;
-	int *org_array = array;
-
-	if (array == NULL || size <= 1)
+	if (size > 1)
 	{
-		return;
-	}
+		size_t partition_index = partition(array, size);
 
-	q_sort(array, size, original_size, org_array);
+		if (partition_index > 0)
+		{
+			quick_sort(array, partition_index);
+		}
+
+		quick_sort(array + partition_index + 1, size - partition_index - 1);
+	}
 }
