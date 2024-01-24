8204788b65437b4099839f48f3f3bf8fe9c324d2
AbuArwa001 <khalfanathman12@yahoo.com>
2024-01-24 04:14:12 +0300

    COcktail sort

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,65 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - swaps two nodes in a doubly linked list
+ * @list: pointer to the head of the list
+ * @left: pointer to the left node
+ * @right: pointer to the right node
+ */
+void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
+{
+    if (left->prev)
+        left->prev->next = right;
+    else
+        *list = right;
+
+    if (right->next)
+        right->next->prev = left;
+
+    left->next = right->next;
+    right->prev = left->prev;
+    left->prev = right;
+    right->next = left;
+}
+
+/**
+ * cocktail_sort_list - sorts a doubly linked list using Cocktail Sort algorithm
+ * @list: double pointer to the head of the list
+ */
+void cocktail_sort_list(listint_t **list)
+{
+    int swapped;
+    listint_t *current;
+
+    if (list == NULL || *list == NULL)
+        return;
+
+    do {
+        swapped = 0;
+        current = *list;
+
+        while (current->next != NULL) {
+            if (current->n > current->next->n) {
+                swap_nodes(list, current, current->next);
+                print_list(*list);
+                swapped = 1;
+            }
+            else
+                current = current->next;
+        }
+
+        if (!swapped)
+            break;
+
+        swapped = 0;
+        while (current->prev != NULL) {
+            if (current->n < current->prev->n) {
+                swap_nodes(list, current->prev, current);
+                print_list(*list);
+                swapped = 1;
+            }
+            else
+                current = current->prev;
+        }
+    } while (swapped);
+}
