8309289e4ada1d97c282ae0d15c57d74218e7bad
AbuArwa001 <khalfanathman12@yahoo.com>
2024-01-28 05:34:03 +0300

    Add radixsort prototype

--- sort.h
+++ sort.h
@@ -83,4 +83,11 @@ void merge_sort(int *array, size_t size);
  * @size: size of the array
 */
 void counting_sort(int *array, size_t size);
+/**
+ * radix_sort - sorts an array of integers
+ *                 in ascending order using the Counting sort algorithm
+ * @array: array to be sorted;
+ * @size: size of the array
+*/
+void radix_sort(int *array, size_t size);
 #endif
